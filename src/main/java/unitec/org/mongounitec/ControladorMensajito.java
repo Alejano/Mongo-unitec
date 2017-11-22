eb4e67cb863d212b4115bfd753672730d8f094f1
T-107 <T-107@PC186>
2017-11-21 21:42:28 -0600

    casi listo servicios

--- src/main/java/unitec/org/mongounitec/ControladorMensajito.java
+++ src/main/java/unitec/org/mongounitec/ControladorMensajito.java
@@ -5,9 +5,12 @@
  */
 package unitec.org.mongounitec;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,19 +20,55 @@ import org.springframework.web.bind.annotation.RestController;
  * @author T-107
  */
 @RestController
-@RequestMapping("/mensaje")
+@RequestMapping("/api")
 @CrossOrigin
 public class ControladorMensajito {
     @Autowired RepositorioMensajito repoMensa;
     //aqui van las 5 operaciones basicas 
     //Buscar 
+    //modificamos abajo
+    /*
     @RequestMapping(value="/buscar",method=RequestMethod.GET,headers ={"Accept=application/json"} )
 public ArrayList<Mensajito> obtenerMensajito()throws Exception{
     return(ArrayList<Mensajito>)  repoMensa.findAll();
-}
+*/
+    
+    //metodo get buscar todos
+    @RequestMapping(value="/mensajito",method=RequestMethod.GET,headers ={"Accept=application/json"} )
+    public ArrayList<Mensajito> obtenerTodos()throws Exception{
+        return(ArrayList<Mensajito>)  repoMensa.findAll();
 
+    }
 
-
+    //metodo get Buscar por id.-este es el uri                     formato de informacion
+    @RequestMapping(value="/mensajito/{id}",method=RequestMethod.GET,headers ={"Accept=application/json"} )
+    public Mensajito obtenerPorId(@PathVariable String id)throws Exception{
+        return  repoMensa.findOne(id);
+    }
+    
+    //Metodo Post guardar version para clientes variables(web y desktop)
+    @RequestMapping(value="/mensajito/{titulo}/{cuerpo}",method=RequestMethod.POST,headers = {"Accept=application/json"})
+    public Estatus guardarMensajito(@PathVariable String titulo,@PathVariable String cuerpo)throws Exception{
+    repoMensa.save(new Mensajito(titulo,cuerpo));
+    Estatus estatus=new Estatus();
+    estatus.setSuccess(true);
+    return estatus;
+    }
+    
+    //el Campitos elige esta 
+    //en esta puede cambiar lo que pide sin modificar la uri
+    //metodo post guardar pero es una version mas pura y efectiva se agrego requestBody
+    @RequestMapping(value="/mensajito",method=RequestMethod.POST,headers = {"Accept=application/json"})
+    public Estatus guardarMensajitoMejorado(@RequestBody String json)throws Exception{
+    
+        //transformamos el json a objeto java 
+        ObjectMapper maper=new ObjectMapper();
+        Mensajito mensa= maper.readValue(json, Mensajito.class);
+        repoMensa.save(mensa);
+        Estatus es=new Estatus();
+        es.setSuccess(true);
+        return es;
+    }
 
 
 }
